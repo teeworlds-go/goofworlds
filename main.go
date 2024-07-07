b478054cc090f3cadee7fea2ccbdfe153c46f244
ChillerDragon <ChillerDragon@gmail.com>
2024-07-07 09:59:51 +0800

    it kinda works xd

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/vector"
+	"github.com/teeworlds-go/protocol/messages7"
+	"github.com/teeworlds-go/protocol/snapshot7"
+	"github.com/teeworlds-go/protocol/teeworlds7"
 )
 
 const (
@@ -14,18 +17,42 @@ const (
 	screenHeight = 480
 )
 
+type Camera struct {
+	X int
+	Y int
+}
+
+type CameraOffset struct {
+	X int
+	Y int
+}
+
+func getCameraOffset(camera Camera) CameraOffset {
+	wc := screenWidth / 2
+	hc := screenHeight / 2
+	x := -camera.X + wc
+	y := -camera.Y + hc
+	return CameraOffset{
+		X: x,
+		Y: y,
+	}
+}
+
 type Game struct {
-	count int
+	Client teeworlds7.Client
+	Camera Camera
 }
 
 func (g *Game) Update() error {
-	if ebiten.IsKeyPressed(ebiten.KeyArrowRight) {
-		g.count++
+	g.Client.Game.Input.Direction = 0
+	if ebiten.IsKeyPressed(ebiten.KeyA) {
+		g.Client.Game.Input.Direction = -1
 	}
-	if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) {
-		g.count--
+	if ebiten.IsKeyPressed(ebiten.KeyD) {
+		g.Client.Game.Input.Direction = 1
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyQ) {
+		g.Client.SendMessage(&messages7.CtrlClose{})
 		os.Exit(0)
 	}
 
@@ -33,7 +60,14 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	vector.DrawFilledRect(screen, 50+float32(g.count), 50, 100, 100, color.RGBA{0x80, 0x80, 0x80, 0xc0}, true)
+	for _, character := range g.Client.Game.Snap.Characters {
+		offset := getCameraOffset(g.Camera)
+
+		screenX := float32(character.X) + float32(offset.X)
+		screenY := float32(character.Y) + float32(offset.Y)
+
+		vector.DrawFilledRect(screen, screenX, screenY, 100, 100, color.RGBA{0x80, 0x80, 0x80, 0xc0}, true)
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
@@ -43,7 +77,24 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 func main() {
 	ebiten.SetWindowSize(screenWidth, screenHeight)
 	ebiten.SetWindowTitle("goofworlds")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+
+	game := &Game{}
+	game.Client = *teeworlds7.NewClient()
+	game.Client.Name = "goofy"
+
+	game.Client.OnSnapshot(func(snap *snapshot7.Snapshot, defaultAction teeworlds7.DefaultAction) {
+		char, err := game.Client.SnapFindCharacter(game.Client.LocalClientId)
+		if err == nil {
+			game.Camera.X = char.X
+			game.Camera.Y = char.Y
+		}
+	})
+
+	go func() {
+		game.Client.Connect("127.0.0.1", 8303)
+	}()
+
+	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
 }
